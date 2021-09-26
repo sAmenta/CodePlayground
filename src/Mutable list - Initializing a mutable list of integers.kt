fcad36a9c4e3a7536a25b253bd9e3e4086ff1bf0
sAmenta <75745860+sAmenta@users.noreply.github.com>
2021-09-27 00:29:51 +0800

    Create an int mutable list named numbers with five elements: 12, 17, 8, 101, and 33. After that, please, output it.

---
+++ src/Mutable list - Initializing a mutable list of integers.kt
@@ -0,0 +1,8 @@
+//Create an int mutable list named numbers with five elements: 12, 17, 8, 101, and 33. After that, please, output it.
+
+fun main() {
+    val numbers = mutableListOf<Int>(12, 17, 8, 101, 33)
+
+        // do not touch the lines below
+        println(numbers.joinToString())
+}
