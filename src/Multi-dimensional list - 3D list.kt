7456ce49faa99ccadab56c318df1b773ce0bb923
sAmenta <s.amenta@protonmail.com>
2022-05-24 23:11:19 +0800

    Create a 3x3x3 three-dimensional list that contains elements of type Int and is filled with zeros (0).
    Print it.

---
+++ src/Multi-dimensional list - 3D list.kt
@@ -0,0 +1,14 @@
+/*
+Create a 3x3x3 three-dimensional list that contains elements of type Int and is filled with zeros (0).
+
+Print it.
+ */
+
+fun main() {
+    val list3D = listOf(
+        listOf(listOf(0, 0, 0), listOf(0, 0, 0), listOf(0, 0, 0)),
+        listOf(listOf(0, 0, 0), listOf(0, 0, 0), listOf(0, 0, 0)),
+        listOf(listOf(0, 0, 0), listOf(0, 0, 0), listOf(0, 0, 0))
+    )
+    print(list3D)
+}
