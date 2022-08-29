80298796745937cbd1fec6a4f07f4ae6048d22d7
Stefano.Amenta <s.amenta@protonmail.com>
2022-08-29 18:15:59 +0800

    https://www.codewars.com/kata/5a00e05cc374cb34d100000d/train/kotlin
    Build a function that returns an array of integers from n to 1 where n>0.
    
    Example : n=5 --> [5,4,3,2,1]

---
+++ src/List - Reversed Sequence.kt
@@ -0,0 +1,7 @@
+/*
+https://www.codewars.com/kata/5a00e05cc374cb34d100000d/train/kotlin
+
+Build a function that returns an array of integers from n to 1 where n>0.
+
+Example : n=5 --> [5,4,3,2,1]
+ */
