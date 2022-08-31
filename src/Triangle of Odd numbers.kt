2bb89d616cd7bebb6ce019f68a335926cdac9dbb
sAmenta <s.amenta@protonmail.com>
2022-09-01 02:53:19 +0800

    Given the triangle of consecutive odd numbers:
                 1
              3     5
           7     9    11
       13    15    17    19
    21    23    25    27    29
    ...
    Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) e.g.: (Input --> Output)
    
    1 -->  1
    2 --> 3 + 5 = 8

---
+++ src/Triangle of Odd numbers.kt
@@ -0,0 +1,31 @@
+/*
+Given the triangle of consecutive odd numbers:
+
+             1
+          3     5
+       7     9    11
+   13    15    17    19
+21    23    25    27    29
+...
+Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) e.g.: (Input --> Output)
+
+1 -->  1
+2 --> 3 + 5 = 8
+
+ */
+
+fun main() {
+    val result = rowSumOddNumbers(readLine()!!.toInt())
+    print(result)
+}
+
+fun rowSumOddNumbers(n: Int): Int {
+    var rowFirstNumber = (n * (n - 1)) + 1
+    var sum = rowFirstNumber
+
+    repeat(n - 1) {
+        rowFirstNumber += 2
+        sum += rowFirstNumber
+    }
+    return sum
+}
